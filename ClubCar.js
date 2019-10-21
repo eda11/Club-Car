d1532826a914d657e74ee0179fe47e2a5320af9a
Ben <bc110@hw.ac.uk>
2019-10-21 15:19:44 +0100

    Improved offscreen collision

--- ClubCar.js
+++ ClubCar.js
@@ -14,6 +14,7 @@ speedX = 0;
 speedY = 0;
 friction = 0.2;
 angle = 0;
+angleSpeed = 0;
 mod = 0;
 angleMod = 0;
 canvas = document.getElementById("gameSpace");
@@ -50,6 +51,9 @@ function draw() {
     context.save();
     context.translate(-x, -y);
     context.drawImage(background,0,0);
+    context.beginPath();
+    context.rect(20, 20, 150, 100);
+    context.stroke();
     context.restore();
 
     //Draw another car
@@ -67,54 +71,76 @@ function draw() {
     context.rotate(angle);
     context.drawImage(img, 10-(img.width), 10-(img.height));
     context.restore();
-    /*
+    
     drawCar(cornerCalcX(x+10,y+10),cornerCalcY(x+10,y+10),
             cornerCalcX(x-30,y+10),cornerCalcY(x-30,y+10),
             cornerCalcX(x-30,y-10),cornerCalcY(x-30,y-10),
             cornerCalcX(x+10,y-10),cornerCalcY(x+10,y-10));
-            */
+            
 
     calculateSpeed();
 }
 
 function calculateSpeed(){
 
-    angle += angleMod*0.03
-
     //Increase the speed in the correct direction
-
+    
+    angleSpeed += angleMod*0.005;
     speedX += accelaration*Math.cos(angle)*mod; 
     speedY += accelaration*Math.sin(angle)*mod;
 
     //Decrease the speeds by the friction
+    angleSpeed = Approach(angleSpeed,0,0.005*Math.abs(angleSpeed/0.06))
     speedX = Approach(speedX, 0, friction*Math.abs(speedX/maxSpeed));
     speedY = Approach(speedY, 0, friction*Math.abs(speedY/maxSpeed));
     //console.log("X :" + Math.abs(friction*Math.sin(angle)) + "    Y : " + Math.abs(friction*Math.sin(angle)))
-    
+
+    c1 = (offScreen(cornerCalcX(x + speedX +10,y + speedY +10,angle + angleSpeed),cornerCalcY(x + speedX +10,y + speedY +10,angle + angleSpeed)))
+    c2 = (offScreen(cornerCalcX(x + speedX -30,y + speedY +10,angle + angleSpeed),cornerCalcY(x + speedX -30,y + speedY +10,angle + angleSpeed)))
+    c3 = (offScreen(cornerCalcX(x + speedX -30,y + speedY -10,angle + angleSpeed),cornerCalcY(x + speedX -30,y + speedY -10,angle + angleSpeed)))
+    c4 = (offScreen(cornerCalcX(x + speedX +10,y + speedY -10,angle + angleSpeed),cornerCalcY(x + speedX +10,y + speedY -10,angle + angleSpeed)))
+
+    console.log(c1)
 
     //Prevents going off screen
-    if ((x+speedX < widthBackground-startx)*(x+speedX > -startx)) {x += speedX}
-    else {speedX = -speedX*0.4; speedY = -speedY*0.4}
-    if ((y+speedY < heightBackground-starty)*(y+speedY > -starty)) {y += speedY}
-    else {speedX = -speedX*0.4; speedY = -speedY*0.4}
+    if (c1*c2*c3*c4) {x += speedX,y += speedY,angle += angleSpeed}
+    else {speedX = -speedX*0.4; speedY = -speedY*0.4; angleSpeed = -angleSpeed*0.4 }
+
+    c1 = (offScreen(cornerCalcX(x + speedX +10,y +10,angle + angleSpeed),cornerCalcY(x + speedX +10,y +10,angle)))
+    c2 = (offScreen(cornerCalcX(x + speedX -30,y +10,angle + angleSpeed),cornerCalcY(x + speedX -30,y +10,angle)))
+    c3 = (offScreen(cornerCalcX(x + speedX -30,y -10,angle + angleSpeed),cornerCalcY(x + speedX -30,y -10,angle)))
+    c4 = (offScreen(cornerCalcX(x + speedX +10,y -10,angle + angleSpeed),cornerCalcY(x + speedX +10,y -10,angle)))
+
+    if (!(c1*c2*c3*c4)){
+        x = Approach(x, startx, 2);
+        y = Approach(y, starty, 2);
+        speedX = Approach(speedX, 0, friction);
+        speedY = Approach(speedY, 0, friction);
+    }
 
 
 }
 
 function drawCar(c1x,c1y,c2x,c2y,c3x,c3y,c4x,c4y) {
-    ctx.beginPath();
-    ctx.moveTo(c1x,c1y);
-    ctx.lineTo(c2x,c2y);
-    ctx.lineTo(c3x,c3y);
-    ctx.lineTo(c4x,c4y);
-    ctx.closePath();
-    ctx.fill();
+    context.beginPath();
+    context.moveTo(c1x,c1y);
+    context.lineTo(c2x,c2y);
+    context.lineTo(c3x,c3y);
+    context.lineTo(c4x,c4y);
+    context.closePath();
+    context.fill();
 }
 
-function cornerCalcX(cornerX,cornerY){
+function offScreen(x,y){
+    if   (((x+speedX < widthBackground-startx)*(x+speedX > -startx))*((y+speedY < heightBackground-starty)*(y+speedY > -starty))) {return true}
+    else {return false}
+
+}
+
+function cornerCalcX(cornerX,cornerY,angle){
     return (Math.cos(angle)*(cornerX-x)) - (Math.sin(angle)*(cornerY-y)) + x;
 }
-function cornerCalcY(cornerX,cornerY){
+function cornerCalcY(cornerX,cornerY,angle){
     return (Math.sin(angle)*(cornerX-x)) + (Math.cos(angle)*(cornerY-y)) + y;
 }
 
@@ -128,7 +154,6 @@ function keyup_handler(event) {
 }
 
 function keypress_handler(event) {
-    console.log(event.keyCode);
     if (event.keyCode == 87) {
         mod = 1;
     }
